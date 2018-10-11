e269eab1385c02283d8d50ed383bef9aed87fc14
blastz <touchzh1996@gmail.com>
2018-10-11 20:56:01 +0800

    Learn relational data

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,3 +1,65 @@
-import testString from './myModule';
+import { GraphQLServer } from 'graphql-yoga';
 
-console.log(testString);
+import { posts, users } from './mock-data';
+
+const typeDefs = `
+  type Query {
+    posts(query: String): [Post!]!
+    users(query: String): [User!]!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+    posts: [Post!]!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+    author: User!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    posts(parent, args, ctx, info) {
+      const { query = null } = args;
+      if (!query) {
+        return posts;
+      } else {
+        return posts.filter(post => {
+          return post.title.toLowerCase().includes(query.toLowerCase()) || post.body.toLowerCase().includes(query.toLowerCase());
+        });
+      }
+    },
+    users(parent, args, ctx, info) {
+      const { query = null } = args;
+      if (!query) {
+        return users;
+      } else {
+        return users.filter(user => user.name.toLowerCase().includes(query.toLowerCase()));
+      }
+    }
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find(user => user.id === parent.author);
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter(post => post.author === parent.id);
+    }
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => {
+  console.log('server start at localhost:4000');
+});
---
+++ graphql-basics/src/mock-data.js
@@ -0,0 +1,44 @@
+const posts = [
+  {
+    id: 'p-1',
+    title: 'Post One',
+    body: 'This is post one',
+    published: false,
+    author: 'u-1'
+  },
+  {
+    id: 'p-2',
+    title: 'Post Two',
+    body: 'This is post two',
+    published: true,
+    author: 'u-1'
+  },
+  {
+    id: 'p-3',
+    title: 'Post Three',
+    body: 'This is post three',
+    published: false,
+    author: 'u-3'
+  }
+];
+
+const users = [
+  {
+    id: 'u-1',
+    name: 'AA',
+    email: 'AAAAAAA@example.com',
+    age: 12
+  },
+  {
+    id: 'u-2',
+    name: 'BB',
+    email: 'BBBBBBB@example.com'
+  },
+  {
+    id: 'u-3',
+    name: 'CC',
+    email: 'CCCCCCC@example.com'
+  }
+];
+
+export { posts, users };
