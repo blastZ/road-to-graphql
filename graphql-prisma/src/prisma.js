b1bb40123443b3fb78bbe0a0c2c61fd9625a3d54
blastz <touchzh1996@gmail.com>
2018-11-01 21:17:38 +0800

    Use prisma in node

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,6 +1,27 @@
 import { Prisma } from 'prisma-binding';
+import path from 'path';
 
 const prisma = new Prisma({
-  typeDefs: './generated/prisma.graphql',
+  typeDefs: path.resolve(__dirname, './generated/prisma.graphql'),
   endpoint: 'http://localhost:4466'
 });
+
+prisma.mutation
+  .updatePost(
+    {
+      where: {
+        id: 'cjnx58v4r000w07813ahk6q7e'
+      },
+      data: {
+        title: 'I like to watch ducks',
+        published: true
+      }
+    },
+    `{ id title published }`
+  )
+  .then(data => {
+    return prisma.query.posts(null, `{ id title body published }`);
+  })
+  .then(data => {
+    console.log(JSON.stringify(data, null, 2));
+  });
